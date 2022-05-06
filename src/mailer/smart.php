d0f5d8a08da0bc55a05e6b171ffdaaed77ef8847
Kenan <haciyevk785@gmail.com>
2022-05-07 02:33:45 +0400

    07.05

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -11,14 +11,14 @@ $mail->CharSet = 'utf-8';
 // $mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
+$mail->Host = 'smtp.yandex.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'kenan.haciyev.2017@mail.ru';                 // Наш логин
-$mail->Password = 'xM3QPuzSkjGQaYvpS99S';                           // Наш пароль от ящика
+$mail->Username = 'malishken@yandex.ru';                 // Наш логин
+$mail->Password = 'klkzfuilzofvnhxa';                          // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('kenan.haciyev.2017@mail.ru', 'Pulse');   // От кого письмо 
+$mail->setFrom('malishken@yandex.ru', 'Pulse');   // От кого письмо 
 $mail->addAddress('gepewa7155@eoscast.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
